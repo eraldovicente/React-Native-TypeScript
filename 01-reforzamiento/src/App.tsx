537125c0ca0277671f77cce63c0d331f818c950b
Eraldo Vicente <eraldovicente2@gmail.com>
2021-04-14 19:49:24 -0300

    Funciones, retorno y argumentos

--- 01-reforzamiento/src/App.tsx
+++ 01-reforzamiento/src/App.tsx
@@ -1,5 +1,7 @@
 // import { TiposBasicos } from "./typescript/TiposBasicos"
-import { ObjetosLiterales } from "./typescript/ObjetosLiterales"
+// import { ObjetosLiterales } from "./typescript/ObjetosLiterales"
+import { Funciones } from "./typescript/Funciones"
+
 
 const App = () => {
   return (
@@ -7,7 +9,8 @@ const App = () => {
       <h1>Introducción a TS - React</h1>
       <hr />
       {/* <TiposBasicos /> */}
-      <ObjetosLiterales />
+      {/* <ObjetosLiterales /> */}
+      <Funciones />
     </div>
   )
 }
