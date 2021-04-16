9f9ee1ece57b89a5b57e390376bb6fe84f3436cf
Eraldo Vicente <eraldovicente2@gmail.com>
2021-04-15 22:41:09 -0300

    Custom Hook - useUsuarios

--- 01-reforzamiento/src/components/Usuarios.tsx
+++ 01-reforzamiento/src/components/Usuarios.tsx
@@ -1,32 +1,9 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { reqResApi } from '../api/reqRes';
-import { ReqResListado, Usuario } from '../interfaces/reqRes';
+import { useUsuarios } from '../hooks/useUsuarios';
+import { Usuario } from '../interfaces/reqRes';
 
 export const Usuarios = () => {
 
-     const [usuarios, setUsuarios] = useState<Usuario[]>([]);
-
-     const paginaRef = useRef(1);
-
-     useEffect(() => {
-          // llamado al API
-          cargarUsuarios();
-     }, []);
-
-     const cargarUsuarios = async () => {
-          const resp = await reqResApi.get<ReqResListado>('/users', {
-               params: {
-                    page: paginaRef.current
-               }
-          })
-
-          if( resp.data.data.length > 0 ) {
-               setUsuarios( resp.data.data );
-               paginaRef.current++;
-          } else {
-               alert('No hay mas registros');
-          }
-     }
+     const { usuarios, cargarUsuarios } = useUsuarios();
 
      const renderItem = ( { id, first_name, last_name, email, avatar }: Usuario ) => {
           return (
@@ -65,6 +42,15 @@ export const Usuarios = () => {
                   </tbody>
              </table>
 
+             <button
+               className="btn btn-primary"
+               onClick={ cargarUsuarios }
+             >
+                  Anteriores
+             </button>
+             
+             &nbsp;
+             
              <button
                className="btn btn-primary"
                onClick={ cargarUsuarios }
