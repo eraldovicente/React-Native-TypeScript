05aa55326c50c5263b1eea522d385b9f4e237f70
Eraldo Vicente <eraldovicente2@gmail.com>
2021-04-16 00:33:23 -0300

    Formularios

---
+++ 01-reforzamiento/src/components/Formularios.tsx
@@ -0,0 +1,40 @@
+import { useState } from "react"
+
+export const Formularios = () => {
+
+     const [ formulario, setFormulario ] = useState({
+          email: 'test@test.com',
+          password: '123456'
+     });
+
+     const onChange = ( value: string, campo: string ) => {
+          setFormulario({
+               ...formulario,
+               [campo]: value
+          });
+     }
+
+     return (
+          <>
+               <h3>Formularios</h3>  
+
+               <input
+                    type="text"
+                    className="form-control"
+                    placeholder={ formulario.email }
+                    onChange={ ({ target }) => onChange( target.value, 'email' )}
+               />
+
+               <input
+                    type="text"
+                    className="form-control mt-2 mb-2"
+                    placeholder={ formulario.password }
+                    onChange={ ({ target }) => onChange( target.value, 'password' )}
+               />
+
+               <code>
+                    <pre>{ JSON.stringify( formulario, null, 2 ) }</pre>
+               </code>
+          </>
+     )
+}
