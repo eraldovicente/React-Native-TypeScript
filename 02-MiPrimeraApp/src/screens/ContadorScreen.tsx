dd0be93e30b3d85528647059c7b45b000bf85e71
Eraldo Vicente <eraldovicente2@gmail.com>
2021-04-16 23:13:58 -0300

    Componente personalizado: Floating Action Button

--- 02-MiPrimeraApp/src/screens/ContadorScreen.tsx
+++ 02-MiPrimeraApp/src/screens/ContadorScreen.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Text, View, TouchableOpacity, StyleSheet } from 'react-native';
+import { Fab } from '../components/Fab';
 
 export const ContadorScreen = () => {
 
@@ -9,25 +10,20 @@ export const ContadorScreen = () => {
           <View style={ styles.container }>
                <Text style={ styles.title }>
                     Contador: { contador }
-               </Text>
+               </Text>               
 
-               <TouchableOpacity
-                    style={ styles.fabLocationBR }
-                    onPress={ () => setContador( contador + 1 ) }
-               >
-                    <View style={ styles.fab }>
-                         <Text style={ styles.fabText }> +1 </Text>
-                    </View>
-               </TouchableOpacity>
+               <Fab 
+                    title="+1"
+               />
 
-               <TouchableOpacity
+               {/* <TouchableOpacity
                     style={ styles.fabLocationBL }
                     onPress={ () => setContador( contador - 1 ) }
                >
                     <View style={ styles.fab }>
                          <Text style={ styles.fabText }> -1 </Text>
                     </View>
-               </TouchableOpacity>
+               </TouchableOpacity> */}
           </View>
      )
 }
@@ -41,28 +37,5 @@ const styles = StyleSheet.create({
           textAlign: 'center',
           fontSize: 40,
           top: -15
-     },
-     fabLocationBR: {
-          position: 'absolute',
-          bottom: 25,
-          right: 25
-     },
-     fabLocationBL: {
-          position: 'absolute',
-          bottom: 25,
-          left: 25
-     },
-     fab: {
-          backgroundColor: '#5856D6',
-          width: 60,
-          height: 60,
-          borderRadius: 100,
-          justifyContent: 'center'
-     },
-     fabText: {
-          color: 'white',
-          fontSize: 25, 
-          fontWeight: 'bold',
-          alignSelf: 'center'
-     }
+     }     
 })
