5afee00d4bc690e75e74a5bb3843e65d371b7d10
Eraldo Vicente <eraldovicente2@gmail.com>
2021-04-17 22:50:31 -0300

    Posicion relativa

--- 02-MiPrimeraApp/App.tsx
+++ 02-MiPrimeraApp/App.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import { SafeAreaView } from 'react-native';
-import { DimensionesScreen } from './src/screens/DimensionesScreen';
+import { PositionScreen } from './src/screens/PositionScreen';
+// import { DimensionesScreen } from './src/screens/DimensionesScreen';
 // import { BoxObjectModelScreen } from './src/screens/BoxObjectModelScreen';
 // import { ContadorScreen } from './src/screens/ContadorScreen';
 // import { HolaMundoScreen } from './src/screens/HolaMundoScreen';
 
 export const App = () => {
   return (
-    <SafeAreaView style={{ height: 400 }}>
+    <SafeAreaView style={{ flex: 1 }}>
       {/* <HolaMundoScreen />
       <ContadorScreen /> */}
       {/* <BoxObjectModelScreen /> */}
-      <DimensionesScreen />
+      {/* <DimensionesScreen /> */}
+      <PositionScreen />
     </SafeAreaView>
   )
 }
