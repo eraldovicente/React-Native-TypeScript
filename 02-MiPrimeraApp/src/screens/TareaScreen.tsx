0a9f369e13c8fdcdb25400d8b62d864e87a2ebd3
Eraldo Vicente <eraldovicente2@gmail.com>
2021-04-19 21:30:22 -0300

    Resolucion de la tarea de disenos

--- 02-MiPrimeraApp/src/screens/TareaScreen.tsx
+++ 02-MiPrimeraApp/src/screens/TareaScreen.tsx
@@ -14,25 +14,34 @@ export const TareaScreen = () => {
 const styles = StyleSheet.create({
     container: {
          flex: 1,
-         backgroundColor: '#28425B'
+         backgroundColor: '#28425B',
+         flexDirection: 'row',
+         justifyContent: 'center',
+         alignItems: 'center'
+
+
     },
     cajaMorada: {
+     //     flex: 2,
+         height: 100, 
          width: 100,
-         height: 100,
          borderWidth: 10,
          borderColor: 'white',
          backgroundColor: '#5856D6'
     },
     cajaNaranja: {
-          width: 100,
+     //     flex: 2,
           height: 100,
+          width: 100,
           borderWidth: 10,
           borderColor: 'white',
-          backgroundColor: '#F0A23B'
+          backgroundColor: '#F0A23B',
+          top: 50
      },
      cajaAzul: {
-          width: 100,
+          // flex: 4,
           height: 100,
+          width: 100,
           borderWidth: 10,
           borderColor: 'white',
           backgroundColor: '#28C4D9'
