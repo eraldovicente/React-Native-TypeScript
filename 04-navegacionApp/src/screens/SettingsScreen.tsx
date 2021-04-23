86942bca9c1e6f88270846649b226551f2530a70
Eraldo Vicente <eraldovicente2@gmail.com>
2021-04-23 14:33:10 -0300

    useSafeAreaInsets

--- 04-navegacionApp/src/screens/SettingsScreen.tsx
+++ 04-navegacionApp/src/screens/SettingsScreen.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import { Text, View } from 'react-native'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import { styles } from '../theme/appTheme';
 
 export const SettingsScreen = () => {
+
+     const insets = useSafeAreaInsets();
+
      return (
-          <View>
-               <Text>Settings Screen</Text>
+          <View style={{ 
+               ...styles.globalMargin,
+               margin: insets.top + 20
+          }}>
+               <Text style={ styles.titulo }>Settings Screen</Text>
           </View>
      )
 }
