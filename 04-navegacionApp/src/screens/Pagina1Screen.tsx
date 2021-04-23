f723d231d1aa263b2953befdeb821fb802417e9a
Eraldo Vicente <eraldovicente2@gmail.com>
2021-04-23 11:49:38 -0300

    Toggle Drawer - Mostrar / Ocultar

--- 04-navegacionApp/src/screens/Pagina1Screen.tsx
+++ 04-navegacionApp/src/screens/Pagina1Screen.tsx
@@ -1,13 +1,25 @@
-import React from 'react';
-import { Button, Text, View } from 'react-native';
+import React, { useEffect } from 'react';
+import { Button, Text, View, TouchableOpacity } from 'react-native';
 import { StackScreenProps } from '@react-navigation/stack';
 import { styles } from '../theme/appTheme';
-import { TouchableOpacity } from 'react-native-gesture-handler';
+import { DrawerScreenProps } from '@react-navigation/drawer';
 
-
-interface Props extends StackScreenProps<any, any>{};
+// interface Props extends StackScreenProps<any, any>{};
+interface Props extends DrawerScreenProps<any, any>{};
 
 export const Pagina1Screen = ( { navigation }: Props ) => {
+
+     useEffect(() => {
+         
+          navigation.setOptions({
+               headerLeft: () => (
+                    <Button 
+                         title="Menú"
+                         onPress={ () => navigation.toggleDrawer() }
+                    />
+               )
+          })          
+     }, [])
 
      return (
           <View style={ styles.globalMargin }>
