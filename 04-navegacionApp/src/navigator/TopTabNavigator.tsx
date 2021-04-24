63e436bc47de3620e82c6d0a5949750426991b7a
Eraldo Vicente <eraldovicente2@gmail.com>
2021-04-24 18:25:55 -0300

    Personalizando el Material Top Tab Navigator

--- 04-navegacionApp/src/navigator/TopTabNavigator.tsx
+++ 04-navegacionApp/src/navigator/TopTabNavigator.tsx
@@ -4,15 +4,58 @@ import { createMaterialTopTabNavigator } from '@react-navigation/material-top-ta
 import { AlbumsScreen } from '../screens/AlbumsScreen';
 import { ContactsScreen } from '../screens/ContactsScreen';
 import { ChatScreen } from '../screens/ChatScreen';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { colores } from '../theme/appTheme';
+import { Text } from 'react-native';
 
 const Tab = createMaterialTopTabNavigator();
 
 export const TopTabNavigator = () => {
-  return (
-    <Tab.Navigator>
-      <Tab.Screen name="ChatScreen" component={ ChatScreen } />
-      <Tab.Screen name="Contacts" component={ ContactsScreen } />
-      <Tab.Screen name="Albums" component={ AlbumsScreen } />
-    </Tab.Navigator>
-  );
+
+     const { top: paddingTop } = useSafeAreaInsets();
+
+     return (
+     <Tab.Navigator
+          style={{ paddingTop }}
+          sceneContainerStyle={{
+               backgroundColor: 'white'
+          }}
+          tabBarOptions={{
+               pressColor: colores.primary,
+               showIcon: true,
+               indicatorStyle: {
+                    backgroundColor: colores.primary
+               },
+               style: {
+                    shadowColor: 'transparent',
+                    elevation: 0
+               }
+          }}
+          screenOptions={ ({ route }) => ({
+          tabBarIcon: ({ color, focused }) => {
+
+               let iconName: string = '';
+               switch( route.name ) {
+                    case 'Chat':
+                         iconName = 'Ch'
+                    break;
+
+                    case 'Contacts':
+                         iconName = 'Co'
+                    break;
+
+                    case 'Albums':
+                         iconName = 'Al'
+                    break;
+               }
+
+               return <Text style={{ color }}>{ iconName }</Text>
+          }
+     })}
+     >
+          <Tab.Screen name="Chat" component={ ChatScreen } />
+          <Tab.Screen name="Contacts" component={ ContactsScreen } />
+          <Tab.Screen name="Albums" component={ AlbumsScreen } />
+     </Tab.Navigator>
+     );
 }
