ccdd06ba86c542d7e3a547b0d817189541a9ef6c
Eraldo Vicente <eraldovicente2@gmail.com>
2021-04-24 22:19:54 -0300

    Iconos en nuestra aplicacion - Instalaciones

--- 04-navegacionApp/src/screens/Tab1Screen.tsx
+++ 04-navegacionApp/src/screens/Tab1Screen.tsx
@@ -1,5 +1,9 @@
 import React, { useEffect } from 'react'
+import Icon from 'react-native-vector-icons/Ionicons';
+
 import { Text, View } from 'react-native'
+import { styles } from '../theme/appTheme';
+
 
 export const Tab1Screen = () => {
 
@@ -8,8 +12,12 @@ export const Tab1Screen = () => {
      }, [])
 
      return (
-          <View>
-               <Text>Tab1Screen</Text>
+          <View style={ styles.globalMargin }>
+               <Text style={ styles.titulo }> Iconos </Text>
+               <Text>
+                    <Icon name="airplane-outline" size={30} color="#900" />
+               </Text>
+
           </View>
      )
 }
