37c3aaafcc540d0ed03053f7e46259c5df896001
Eraldo Vicente <eraldovicente2@gmail.com>
2021-04-24 22:39:59 -0300

    Instalacion de iconos en IOS

--- 04-navegacionApp/src/screens/Tab1Screen.tsx
+++ 04-navegacionApp/src/screens/Tab1Screen.tsx
@@ -3,19 +3,30 @@ import Icon from 'react-native-vector-icons/Ionicons';
 
 import { Text, View } from 'react-native'
 import { styles } from '../theme/appTheme';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 
 export const Tab1Screen = () => {
+
+     const { top } = useSafeAreaInsets();
 
      useEffect(() => {
           console.log('Tab1Screen effect');               
      }, [])
 
      return (
-          <View style={ styles.globalMargin }>
+          <View style={{ 
+               ...styles.globalMargin,
+               marginTop: top + 10
+          }}>
                <Text style={ styles.titulo }> Iconos </Text>
                <Text>
-                    <Icon name="airplane-outline" size={30} color="#900" />
+                    <Icon name="leaf-outline" size={80} color="#900" />
+                    <Icon name="leaf-outline" size={80} color="#900" />
+                    <Icon name="leaf-outline" size={80} color="#900" />
+                    <Icon name="leaf-outline" size={80} color="#900" />
+                    <Icon name="leaf-outline" size={80} color="#900" />
+                    <Icon name="leaf-outline" size={80} color="#900" />
                </Text>
 
           </View>
