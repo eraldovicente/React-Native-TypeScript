b9708832ff4a76556135fcc061c9e7d8d574f7ee
Eraldo Vicente <eraldovicente2@gmail.com>
2021-04-28 19:36:25 -0300

    Custom Component

--- 04-navegacionApp/src/screens/Tab1Screen.tsx
+++ 04-navegacionApp/src/screens/Tab1Screen.tsx
@@ -1,9 +1,9 @@
-import React, { useEffect } from 'react'
-import Icon from 'react-native-vector-icons/Ionicons';
+import React, { useEffect } from 'react';
 
-import { Text, View } from 'react-native'
+import { Text, View } from 'react-native';
 import { styles } from '../theme/appTheme';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { TouchableIcon } from '../components/TouchableIcon';
 
 
 export const Tab1Screen = () => {
@@ -21,12 +21,12 @@ export const Tab1Screen = () => {
           }}>
                <Text style={ styles.titulo }> Iconos </Text>
                <Text>
-                    <Icon name="leaf-outline" size={80} color="#900" />
-                    <Icon name="leaf-outline" size={80} color="#900" />
-                    <Icon name="leaf-outline" size={80} color="#900" />
-                    <Icon name="leaf-outline" size={80} color="#900" />
-                    <Icon name="leaf-outline" size={80} color="#900" />
-                    <Icon name="leaf-outline" size={80} color="#900" />
+                    <TouchableIcon iconName="airplane-outline" />
+                    <TouchableIcon iconName="attach-outline" />
+                    <TouchableIcon iconName="bonfire-outline" />
+                    <TouchableIcon iconName="calculator-outline" />
+                    <TouchableIcon iconName="images-outline" />
+                    <TouchableIcon iconName="leaf-outline" />
                </Text>
 
           </View>
