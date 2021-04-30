6694931a25bf7056b0d1120fe07a234b4b812d97
Eraldo Vicente <eraldovicente2@gmail.com>
2021-04-29 22:24:45 -0300

    Tipado de TheMovieDB

--- 05-PeliculasApp/src/screens/HomeScreen.tsx
+++ 05-PeliculasApp/src/screens/HomeScreen.tsx
@@ -1,14 +1,15 @@
 import React, { useEffect } from 'react';
 import { Button, Text, View } from 'react-native';
 import movieDB from '../api/movieDB';
+import { MovieDBNowPlaying } from '../interfaces/movieInterface';
 
 export const HomeScreen = () => {
 
      useEffect(() => {
 
-          movieDB.get('/now_playing')
+          movieDB.get<MovieDBNowPlaying>('/now_playing')
                .then( resp => {
-                    console.log(resp)
+                    console.log(resp.data.results[0].title);
                });
      }, []);
 
