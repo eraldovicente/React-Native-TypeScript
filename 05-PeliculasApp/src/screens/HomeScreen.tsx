6f540c96550d52832fa36ca932a40af0c2382b69
Eraldo Vicente <eraldovicente2@gmail.com>
2021-05-04 18:28:43 -0300

    Usar los colores del context

--- 05-PeliculasApp/src/screens/HomeScreen.tsx
+++ 05-PeliculasApp/src/screens/HomeScreen.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { ActivityIndicator, Dimensions, ScrollView, View } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import Carousel from 'react-native-snap-carousel';
 import { GradientBackground } from '../components/GradientBackground';
 import { HorizontalSlider } from '../components/HorizontalSlider';
 import { MoviePoster } from '../components/MoviePoster';
+import { GradientContext } from '../context/GradientContext';
 import { getImageColors } from '../helpers/getColores';
 import { useMovies } from '../hooks/useMovies';
 
@@ -14,15 +15,15 @@ export const HomeScreen = () => {
 
      const { nowPlaying, popular, topRated, upcoming, isLoading } = useMovies();
      const { top } = useSafeAreaInsets();
+     const { setMainColors } = useContext( GradientContext );
 
      const getPosterColors = async( index: number ) => {
           const movie = nowPlaying[index];
           const uri = `https://image.tmdb.org/t/p/w500${ movie.poster_path }`;
 
-          const [ primary, secondary ] = await getImageColors( uri);
+          const [ primary = 'green', secondary = 'orange' ] = await getImageColors( uri);
 
-          console.log({ primary, secondary });
-          
+          setMainColors({ primary, secondary });      
           
      }
 
