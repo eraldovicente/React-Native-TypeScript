bba2b567bf81841ad2c89abd470a79fb1da31c47
Eraldo Vicente <eraldovicente2@gmail.com>
2021-05-04 17:01:49 -0300

    Obtener los colores de las imagenes

--- 05-PeliculasApp/src/screens/HomeScreen.tsx
+++ 05-PeliculasApp/src/screens/HomeScreen.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { ActivityIndicator, Dimensions, ScrollView, View } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import Carousel from 'react-native-snap-carousel';
+import ImageColors from "react-native-image-colors"
 import { GradientBackground } from '../components/GradientBackground';
 import { HorizontalSlider } from '../components/HorizontalSlider';
 import { MoviePoster } from '../components/MoviePoster';
@@ -13,6 +14,16 @@ export const HomeScreen = () => {
 
      const { nowPlaying, popular, topRated, upcoming, isLoading } = useMovies();
      const { top } = useSafeAreaInsets();
+
+     const getPosterColors = async( index: number ) => {
+          const movie = nowPlaying[index];
+          const uri = `https://image.tmdb.org/t/p/w500${ movie.poster_path }`;
+
+          const colors = await ImageColors.getColors(uri, {})
+
+          console.log( colors );
+          
+     }
 
      if ( isLoading ) {
           return (
@@ -36,6 +47,7 @@ export const HomeScreen = () => {
                                    sliderWidth={ windowWidth }
                                    itemWidth={ 230 }
                                    inactiveSlideOpacity={ 0.9 }
+                                   onSnapToItem={ index => getPosterColors( index ) }
                               />
                          </View>
 
