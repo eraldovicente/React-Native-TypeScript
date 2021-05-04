297e23574b953ebb4e74ef4a66f0a8a536bf277f
Eraldo Vicente <eraldovicente2@gmail.com>
2021-05-04 16:13:02 -0300

    Fondo con gradiente

--- 05-PeliculasApp/src/screens/HomeScreen.tsx
+++ 05-PeliculasApp/src/screens/HomeScreen.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { ActivityIndicator, Dimensions, ScrollView, View } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import Carousel from 'react-native-snap-carousel';
+import { GradientBackground } from '../components/GradientBackground';
 import { HorizontalSlider } from '../components/HorizontalSlider';
 import { MoviePoster } from '../components/MoviePoster';
 import { useMovies } from '../hooks/useMovies';
@@ -22,27 +23,31 @@ export const HomeScreen = () => {
      }
 
      return (
+          <GradientBackground>
 
-          <ScrollView>
-               <View style={{ marginTop: top + 20 }}>
+               <ScrollView>
+                    <View style={{ marginTop: top + 20 }}>
 
-                    {/* Carosel Principal */}
-                    <View style={{ height: 340 }}>
-                         <Carousel
-                              data={ nowPlaying }
-                              renderItem={ ({ item }: any) => <MoviePoster movie={ item } /> }
-                              sliderWidth={ windowWidth }
-                              itemWidth={ 230 }
-                              inactiveSlideOpacity={ 0.9 }
-                         />
+                         {/* Carosel Principal */}
+                         <View style={{ height: 340 }}>
+                              <Carousel
+                                   data={ nowPlaying }
+                                   renderItem={ ({ item }: any) => <MoviePoster movie={ item } /> }
+                                   sliderWidth={ windowWidth }
+                                   itemWidth={ 230 }
+                                   inactiveSlideOpacity={ 0.9 }
+                              />
+                         </View>
+
+                         {/* Películas  populares */}
+                         <HorizontalSlider title="En cine" movies={ popular } />
+                         <HorizontalSlider title="Top Rated" movies={ topRated } />
+                         <HorizontalSlider title="UpComing" movies={ upcoming } />
+
                     </View>
+               </ScrollView>
 
-                    {/* Películas  populares */}
-                    <HorizontalSlider title="En cine" movies={ popular } />
-                    <HorizontalSlider title="Top Rated" movies={ topRated } />
-                    <HorizontalSlider title="UpComing" movies={ upcoming } />
+          </GradientBackground>
 
-               </View>
-          </ScrollView>
      )
 }
