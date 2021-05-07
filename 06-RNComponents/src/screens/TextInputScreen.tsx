eb51c83460e00c12558f63428b423c31a0f5ef75
Eraldo Vicente <eraldovicente2@gmail.com>
2021-05-07 17:37:27 -0300

    Multiples TextInputs en pantalla

--- 06-RNComponents/src/screens/TextInputScreen.tsx
+++ 06-RNComponents/src/screens/TextInputScreen.tsx
@@ -1,16 +1,53 @@
-import React from 'react'
-import { StyleSheet, TextInput, View } from 'react-native'
-import { HeaderTitle } from '../components/HeaderTitle'
-import { styles } from '../theme/appTheme'
+import React, { useState } from 'react';
+import { StyleSheet, TextInput, View } from 'react-native';
+import { HeaderTitle } from '../components/HeaderTitle';
+import { styles } from '../theme/appTheme';
 
 export const TextInputScreen = () => {
+
+     const [form, setForm] = useState({
+          name: '',
+          email: '',
+          phone: ''
+     });
+
+     const onChange = ( value: string, field: string ) => {
+          setForm({
+               ...form,
+               [field]: value
+          })
+     }
+
      return (
           <View style={ styles.globalMargin }>
                <HeaderTitle title="TextInputs" />
 
                <TextInput
                     style={ stylesScreen.inputStyle }
+                    placeholder="Ingrese su nombre"
+                    autoCorrect={ false }
+                    autoCapitalize="words"
+                    onChangeText={ ( value ) => onChange( value, 'name') }
                />
+
+               <TextInput
+                    style={ stylesScreen.inputStyle }
+                    placeholder="Ingrese su email"
+                    autoCorrect={ false }
+                    autoCapitalize="none"
+                    onChangeText={ ( value ) => onChange( value, 'email') }
+                    keyboardType="email-address"
+                    keyboardAppearance="dark"
+               />
+
+               <TextInput
+                    style={ stylesScreen.inputStyle }
+                    placeholder="Ingrese su teléfono"
+                    onChangeText={ ( value ) => onChange( value, 'phone') }
+                    keyboardType="phone-pad"
+               />
+
+               <HeaderTitle title={ JSON.stringify( form, null, 3 ) } />
 
           </View>
      )
@@ -22,6 +59,7 @@ const stylesScreen = StyleSheet.create({
           borderColor: 'rgba(0, 0, 0, 0.5)',
           height: 50,
           paddingHorizontal: 10,
-          borderRadius: 10
+          borderRadius: 10,
+          marginVertical: 10
      }
 });
