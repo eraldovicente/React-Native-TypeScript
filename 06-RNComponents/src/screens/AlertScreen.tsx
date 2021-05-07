dab6230029336816b2a3c3324141bea4c609aa9f
Eraldo Vicente <eraldovicente2@gmail.com>
2021-05-07 16:27:15 -0300

    Prompt IOS y Android

--- 06-RNComponents/src/screens/AlertScreen.tsx
+++ 06-RNComponents/src/screens/AlertScreen.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Alert, Button, View } from 'react-native';
+
+import prompt from 'react-native-prompt-android';
+
 import { HeaderTitle } from '../components/HeaderTitle';
 import { styles } from '../theme/appTheme';
 
@@ -28,14 +31,28 @@ export const AlertScreen = () => {
 
      const showPrompt = () => {
 
-          Alert.prompt(
-               '?Está seguro?',
-               'Esta acción no se puede revertir',
-               ( valor: string ) => console.log('info: ', valor ),
-               'plain-text',
-               'Hola Mundo',
-               'number-pad'
-          )
+          // Alert.prompt(
+          //      '?Está seguro?',
+          //      'Esta acción no se puede revertir',
+          //      ( valor: string ) => console.log('info: ', valor ),
+          //      'plain-text',
+          //      'Hola Mundo',
+          //      'number-pad'
+          // )
+          prompt(
+               'Enter password',
+               'Enter your password to claim your $1.5B in lottery winnings',
+               [
+                {text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
+                {text: 'OK', onPress: password => console.log('OK Pressed, password: ' + password)},
+               ],
+               {
+                   type: 'secure-text',
+                   cancelable: false,
+                   defaultValue: 'test',
+                   placeholder: 'placeholder'
+               }
+           );
      }
 
      return (
@@ -46,6 +63,8 @@ export const AlertScreen = () => {
                     title="Mostrar Alerta"
                     onPress={ showAlert }
                />
+
+               <View style={{ height: 10 }} />
 
                <Button
                     title="Mostrar Alerta"
