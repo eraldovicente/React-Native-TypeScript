4e57ba6521452bf832cdc43ff93285c95efd6817
Eraldo Vicente <eraldovicente2@gmail.com>
2021-05-07 19:34:25 -0300

    Personalizando el RefreshControl

--- 06-RNComponents/src/screens/PullToRefreshScreen.tsx
+++ 06-RNComponents/src/screens/PullToRefreshScreen.tsx
@@ -1,11 +1,15 @@
 import React, { useState } from 'react'
 import { ScrollView, View, RefreshControl } from 'react-native'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import { HeaderTitle } from '../components/HeaderTitle'
 import { styles } from '../theme/appTheme'
 
 export const PullToRefreshScreen = () => {
 
+     // const { top } = useSafeAreaInsets();
+
      const [ refreshing, setRefreshing ] = useState(false);
+     const [ data, setData ] = useState<string>();
 
      const onRefresh = () => {
           setRefreshing(true);
@@ -13,20 +17,36 @@ export const PullToRefreshScreen = () => {
           setTimeout(() => {
                console.log('Terminamos');
                setRefreshing(false);
-          }, 1500);
+               setData('Hola mundo');
+          }, 2500);
      }
 
      return (
           <ScrollView
+               style={{
+                    // marginTop: refreshing ? top : 0
+               }}
                refreshControl={
                     <RefreshControl
                          refreshing={ refreshing }
                          onRefresh={ onRefresh }
+                         progressViewOffset={ 10 }
+                         progressBackgroundColor="#5856D6"
+                         colors={ ['white', 'red', 'orange'] }
+                         // style={{ backgroundColor: '#5856D6' }}
+                         // tintColor="white"
+                         // title="Refreshing"
+                         // titleColor="white"
                     />
                }
           >
                <View style={ styles.globalMargin }>
-                    <HeaderTitle title="Pull to refresh" />     
+                    <HeaderTitle title="Pull to refresh" />    
+
+                    {
+                         data && <HeaderTitle title={ data } /> 
+                    } 
+                        
                </View>
           </ScrollView>
      )
