e4daf4855d31e4c3f8346c0d87b7910308c0d602
Eraldo Vicente <eraldovicente2@gmail.com>
2021-05-07 15:43:52 -0300

    Componente - Alert

--- 06-RNComponents/navigator/Navigator.tsx
+++ 06-RNComponents/navigator/Navigator.tsx
@@ -4,6 +4,7 @@ import { HomeScreen } from '../src/screens/HomeScreen';
 import { Animation101Screen } from '../src/screens/Animation101Screen';
 import { Animation102Screen } from '../src/screens/Animation102Screen';
 import { SwitchScreen } from '../src/screens/SwitchScreen';
+import { AlertScreen } from '../src/screens/AlertScreen';
 
 const Stack = createStackNavigator();
 
@@ -18,6 +19,7 @@ export const Navigator = () => {
           <Stack.Screen name="Animation101Screen" component={ Animation101Screen } />
           <Stack.Screen name="Animation102Screen" component={ Animation102Screen } />
           <Stack.Screen name="SwitchScreen" component={  SwitchScreen  } />
+          <Stack.Screen name="AlertScreen" component={  AlertScreen  } />
      </Stack.Navigator>
      );
 }
