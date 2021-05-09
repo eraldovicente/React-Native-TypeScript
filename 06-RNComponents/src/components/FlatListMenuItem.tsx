5d5830ac3253329991ee22149ecad8283d8bff0d
Eraldo Vicente <eraldovicente2@gmail.com>
2021-05-09 16:13:47 -0300

    Theme Light y Dark - Generalidades

--- 06-RNComponents/src/components/FlatListMenuItem.tsx
+++ 06-RNComponents/src/components/FlatListMenuItem.tsx
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import { MenuItem } from '../interfaces/appInterfaces';
 import  Icon  from 'react-native-vector-icons/Ionicons';
 import { useNavigation } from '@react-navigation/core';
+import { useTheme } from '@react-navigation/native';
 
 interface Props {
      menuItem: MenuItem
@@ -11,6 +12,7 @@ interface Props {
 export const FlatListMenuItem = ({ menuItem }: Props ) => {
 
      const navigation = useNavigation();
+     const { colors } = useTheme();
 
      return (
           <TouchableOpacity
@@ -24,7 +26,10 @@ export const FlatListMenuItem = ({ menuItem }: Props ) => {
                          size={ 27 }
                     />
 
-                    <Text style={ styles.itemText }>
+                    <Text style={{ 
+                         ...styles.itemText,
+                         color: colors.text
+                    }}>
                          { menuItem.name }
                     </Text>
 
