eb80ecf7e69d8e7f1cad7ef26142a597dcdf24d9
Eraldo Vicente <eraldovicente2@gmail.com>
2021-05-09 13:53:53 -0300

    Boton en el ultimo Slide

--- 06-RNComponents/src/screens/SlidesScreen.tsx
+++ 06-RNComponents/src/screens/SlidesScreen.tsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
-import { View, SafeAreaView, ImageSourcePropType, Text, Dimensions, Image, StyleSheet  } from 'react-native';
+import { TouchableOpacity, View, SafeAreaView, ImageSourcePropType, Text, Dimensions, Image, StyleSheet  } from 'react-native';
 
 import Carousel, { Pagination } from 'react-native-snap-carousel';
+import Icon from 'react-native-vector-icons/Ionicons';
+
 
 const { height: screenHeight, width: screenWidth } = Dimensions.get('window');
 
@@ -77,16 +79,48 @@ export const SlidesScreen = () => {
                     }}
                />
 
-               <Pagination
-                    dotsLength={ items.length }
-                    activeDotIndex={ activeIndex }
-                    dotStyle={{
-                         width: 10,
-                         height: 10,
-                         borderRadius: 10,
-                         backgroundColor: '#5856D6'
+               <View style={{
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                    marginHorizontal: 20,
+                    alignItems: 'center'
+               }}>
+                    <Pagination
+                         dotsLength={ items.length }
+                         activeDotIndex={ activeIndex }
+                         dotStyle={{
+                              width: 10,
+                              height: 10,
+                              borderRadius: 10,
+                              backgroundColor: '#5856D6'
+                         }}
+                    />
+
+                    <TouchableOpacity style={{
+                         flexDirection: 'row',
+                         backgroundColor: "#5856D6",
+                         width: 120,
+                         height: 40,
+                         borderRadius: 25,
+                         justifyContent: 'center',
+                         alignItems: 'center',
+                         paddingLeft: 10
                     }}
-               />
+                         activeOpacity={ 0.8 }
+                    >
+                         <Text style={{
+                              fontSize: 20,
+                              color: 'white'
+                         }}>
+                              Entrar
+                         </Text>
+                         <Icon
+                              name="chevron-forward-outline"
+                              color="white"
+                              size={ 30 }
+                         />
+                    </TouchableOpacity>
+               </View>
 
           </SafeAreaView>
      )
