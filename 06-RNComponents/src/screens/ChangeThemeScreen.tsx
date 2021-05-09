b5878776330aeb70134d5d82e04c69b658af3d1a
Eraldo Vicente <eraldovicente2@gmail.com>
2021-05-09 19:56:30 -0300

    ThemeReducer

--- 06-RNComponents/src/screens/ChangeThemeScreen.tsx
+++ 06-RNComponents/src/screens/ChangeThemeScreen.tsx
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { View, TouchableOpacity, Text } from 'react-native';
 import { HeaderTitle } from '../components/HeaderTitle';
 import { styles } from '../theme/appTheme';
+import { ThemeContext } from '../context/themeContext/ThemeContext';
 
 export const ChangeThemeScreen = () => {
+
+     const { setDarkTheme } = useContext( ThemeContext );
+
      return (
           <View style={ styles.globalMargin }>
                <HeaderTitle title="Theme" />
 
                <TouchableOpacity 
+                    onPress={ setDarkTheme }
                     activeOpacity={ 0.8 }
                     style={{
                          width: 150,
