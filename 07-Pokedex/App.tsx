db4c30d22149bfe45ddaa75eec4946e0a76830e2
Eraldo Vicente <eraldovicente2@gmail.com>
2021-05-10 17:12:28 -0300

    Stack Navigator e iconos

---
+++ 07-Pokedex/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { Navigator } from './src/navigator/Navigator';
+
+export const App = () => {
+  return (
+    <NavigationContainer>
+      <Navigator />
+    </NavigationContainer>
+  )
+}
+
+export default App;
