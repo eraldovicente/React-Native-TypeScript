970de06de2688f29e4cb87bb220a70b45726e5e6
Eraldo Vicente <eraldovicente2@gmail.com>
2021-05-11 18:49:59 -0300

    FadeInImage y useAnimation

--- 07-Pokedex/src/screens/HomeScreen.tsx
+++ 07-Pokedex/src/screens/HomeScreen.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { ActivityIndicator, FlatList, Image, Text } from 'react-native'
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { FadeInImage } from '../components/FadeInImage';
 import { usePokemonPaginated } from '../hooks/usePokemonPaginated';
 import { styles } from '../theme/appTheme'
 
@@ -21,8 +22,8 @@ export const HomeScreen = () => {
                     keyExtractor={ ( pokemon ) => pokemon.id }
                     showsVerticalScrollIndicator={ false }
                     renderItem={ ({ item }) => (
-                         <Image 
-                              source={{ uri: item.picture }}
+                         <FadeInImage
+                              uri={ item.picture }
                               style={{
                                    width: 100,
                                    height: 100
