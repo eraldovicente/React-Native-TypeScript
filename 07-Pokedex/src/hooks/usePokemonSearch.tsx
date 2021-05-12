2844306667cbba9b7c8ef412fc5fa7d03072685e
Eraldo Vicente <eraldovicente2@gmail.com>
2021-05-12 17:27:22 -0300

    Pre-cargar la informacion para buscar por nombre

---
+++ 07-Pokedex/src/hooks/usePokemonSearch.tsx
@@ -0,0 +1,43 @@
+import { useEffect, useRef, useState } from "react";
+import { pokemonApi } from "../api/pokemonApi";
+import { PokemonPaginatedResponse, Result, SimplePokemon } from "../interfaces/pokemonInterfaces";
+
+
+export const usePokemonSearch = () => {
+
+     const [ isFetching, setisFetching ] = useState(true);
+     const [ simplePokemonList, setSimplePokemonList ] = useState<SimplePokemon[]>([]);
+
+     const loadPokemons = async() => {
+
+          const resp = await pokemonApi.get<PokemonPaginatedResponse>( 'https://pokeapi.co/api/v2/pokemon?limit=1200' );
+          mapPokemonList( resp.data.results );
+          
+     }
+     
+     const mapPokemonList = ( pokemonList: Result[] ) => {
+
+          const newPokemonList: SimplePokemon[] = pokemonList.map(({ name, url }) => {
+
+               const urlParst = url.split('/');
+               const id = urlParst[ urlParst.length - 2 ];
+               const picture = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${ id }.png`;
+
+               return { id, picture, name };
+
+          });
+
+          setSimplePokemonList( newPokemonList );
+          setisFetching( false );
+     }
+
+     useEffect(() => {
+         loadPokemons(); 
+     }, []);
+
+     return {
+          isFetching,
+          simplePokemonList
+     }
+
+}
