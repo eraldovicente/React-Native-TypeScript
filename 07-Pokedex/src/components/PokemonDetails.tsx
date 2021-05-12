03b10994da9604b16c271c41fa0d0e745f183466
Eraldo Vicente <eraldovicente2@gmail.com>
2021-05-12 02:20:37 -0300

    Peso y Sprites del Pokemon

--- 07-Pokedex/src/components/PokemonDetails.tsx
+++ 07-Pokedex/src/components/PokemonDetails.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { ScrollView, StyleSheet, Text, View } from 'react-native';
 import { PokemonFull } from '../interfaces/pokemonInterfaces';
+import { FadeInImage } from './FadeInImage';
 
 interface Props {
      pokemon: PokemonFull;
@@ -10,11 +11,12 @@ export const PokemonDetails = ({ pokemon }: Props ) => {
 
      return (
           <ScrollView
+               showsVerticalScrollIndicator={ false }
                style={{
                     ...StyleSheet.absoluteFillObject
                }}
           >
-               {/* Types */}
+               {/* Types y peso */}
                <View style={{
                     ...styles.container,
                     marginTop: 380
@@ -35,14 +37,81 @@ export const PokemonDetails = ({ pokemon }: Props ) => {
                               ))
                          }
                     </View>
+                    
+                    {/* Peso */}
+                    <Text style={ styles.title }>Peso</Text>
+                    <Text style={ styles.regularText }>{ pokemon.weight }lb</Text>
                </View>
                
                {/* Sprits */}
-               <View style={{
-                    ...styles.container,
-                    marginTop: 20
-               }}>
+               <View style={ styles.container }>
                     <Text style={ styles.title }>Sprites</Text>   
+               </View>
+               
+               <ScrollView
+                    horizontal={ true }
+                    showsHorizontalScrollIndicator={ false }
+               >
+
+                    <FadeInImage
+                         uri={ pokemon.sprites.front_default }
+                         style={ styles.basicSprite }
+                    />
+
+                    <FadeInImage
+                         uri={ pokemon.sprites.back_default }
+                         style={ styles.basicSprite }
+                    />
+
+                    <FadeInImage
+                         uri={ pokemon.sprites.front_shiny }
+                         style={ styles.basicSprite }
+                    />
+
+                    <FadeInImage
+                         uri={ pokemon.sprites.back_shiny }
+                         style={ styles.basicSprite }
+                    />
+               </ScrollView>
+               
+               {/* Habilidades */}
+               <View style={ styles.container }>
+                    <Text style={ styles.title }>Habilidades base</Text>
+                    <View style={{ flexDirection: 'row' }}>
+                         {
+                              pokemon.abilities.map( ({ ability }) => (
+                                   <Text
+                                   style={{ 
+                                        ...styles.regularText,
+                                        marginRight: 10
+                                   }}
+                                        key={ ability.name }
+                                   >
+                                        { ability.name }
+                                   </Text>
+                              ))
+                         }
+                    </View>
+               </View>
+
+               {/* Movimientos */}
+               <View style={ styles.container }>
+                    <Text style={ styles.title }>Movimientos</Text>
+                    <View style={{ flexWrap: 'wrap', flexDirection: 'row' }}>
+                         {
+                              pokemon.moves.map( ({ move }) => (
+                                   <Text
+                                   style={{ 
+                                        ...styles.regularText,
+                                        marginRight: 10
+                                   }}
+                                        key={ move.name }
+                                   >
+                                        { move.name }
+                                   </Text>
+                              ))
+                         }
+                    </View>
                </View>
 
           </ScrollView>
@@ -55,9 +124,14 @@ const styles = StyleSheet.create({
      },
      title: {
           fontSize: 20,
-          fontWeight: 'bold'
+          fontWeight: 'bold',
+          marginTop: 20
      },
      regularText: {
           fontSize: 17
+     },
+     basicSprite: {
+          width: 100,
+          height: 100
      }
 });
