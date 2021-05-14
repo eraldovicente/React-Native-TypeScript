473fc0bf0e17b6b390c007ff5c693271bdb724ef
Eraldo Vicente <eraldovicente2@gmail.com>
2021-05-14 00:53:27 -0300

    Uso del PermissionContext

--- 08-RutasApp/src/context/PermissionsContext.tsx
+++ 08-RutasApp/src/context/PermissionsContext.tsx
@@ -1,5 +1,6 @@
 import React, { createContext, useState } from 'react';
-import { PermissionStatus } from 'react-native-permissions';
+import { Platform } from 'react-native';
+import { PermissionStatus, request, PERMISSIONS } from 'react-native-permissions';
 
 export interface PermissionsState {
      locationStatus: PermissionStatus
@@ -19,15 +20,31 @@ export const PermissionsContext = createContext({} as PermissionsContextProps );
 
 export const PermissionsProvider = ({ children }: any ) => {
 
-     const askLocationPermission = () => {
+     const [ permissions, setPermissions ] = useState( permissionInitState );
 
+     const askLocationPermission = async() => {
+          let permissionStatus: PermissionStatus;
+
+          if ( Platform.OS === 'ios' ) {
+
+               permissionStatus = await request( PERMISSIONS.IOS.LOCATION_WHEN_IN_USE );
+
+          } else {
+
+               permissionStatus = await request( PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION );
+
+          }   
+
+          setPermissions({
+               ...permissions,
+               locationStatus: permissionStatus
+          });
      }
 
      const checkLocationPermission = () => {
 
      }     
 
-     const [ permissions, setPermissions ] = useState( permissionInitState )
 
      return(
           <PermissionsContext.Provider value={{
--- 08-RutasApp/src/pages/PermissionsScreen.tsx
+++ 08-RutasApp/src/pages/PermissionsScreen.tsx
@@ -1,27 +1,11 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Text, View, StyleSheet, Button, Platform } from 'react-native';
 import { check, PERMISSIONS, PermissionStatus, request } from 'react-native-permissions';
+import { PermissionsContext } from '../context/PermissionsContext';
 
 export const PermissionsScreen = () => {
 
-     const checkLocationPermission = async() => {
-
-          let permissionStatus: PermissionStatus;
-
-          if ( Platform.OS === 'ios' ) {
-
-               // permissionStatus = await check( PERMISSIONS.IOS.LOCATION_WHEN_IN_USE );
-               permissionStatus = await request( PERMISSIONS.IOS.LOCATION_WHEN_IN_USE );
-
-          } else {
-
-               // permissionStatus = await check( PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION );
-               permissionStatus = await request( PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION );
-          }
-          console.log({ permissionStatus });
-          
-
-     }
+     const { permissions, askLocationPermission } = useContext( PermissionsContext );
 
      return (
           <View style={ styles.container }>
@@ -29,8 +13,12 @@ export const PermissionsScreen = () => {
 
                <Button
                     title="Permiso"
-                    onPress={ checkLocationPermission }
+                    onPress={ askLocationPermission }
                />
+
+               <Text>
+                    { JSON.stringify( permissions, null, 5 ) }
+               </Text>
           </View>
      )
 }
