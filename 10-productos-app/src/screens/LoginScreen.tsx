ebe5cd90f3dad22cf54645ef4b79ff4e695ec307
Eraldo Vicente <eraldovicente2@gmail.com>
2021-05-18 19:38:29 -0300

    Diseno del login - Parte 2

--- 10-productos-app/src/screens/LoginScreen.tsx
+++ 10-productos-app/src/screens/LoginScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, TextInput } from 'react-native';
+import { Platform, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import { Background } from '../components/Background';
 import { WhiteLogo } from '../components/WhiteLogo';
 import { loginStyles } from '../theme/loginTheme';
@@ -9,17 +9,70 @@ export const LoginScreen = () => {
           <>
                {/* Background */}
                <Background />
-               {/* Keyboard avoid view */}
-               <WhiteLogo />
 
-               <Text style={ loginStyles.title }>Login</Text>
+               <View style={ loginStyles.formContainer }>
 
-               <Text style={ loginStyles.label }>Email</Text>
-               <TextInput
-                    placeholder="Ingrese su email"
-                    placeholderTextColor="rgba(255,255,255,0.5)"
-                    keyboardType="email-address"
-               />
+                    {/* Keyboard avoid view */}
+                    <WhiteLogo />
+
+                    <Text style={ loginStyles.title }>Login</Text>
+
+                    <Text style={ loginStyles.label }>Email</Text>
+                    <TextInput
+                         placeholder="Ingrese su email"
+                         placeholderTextColor="rgba(255,255,255,0.5)"
+                         keyboardType="email-address"
+                         underlineColorAndroid="white"
+                         style={[ 
+                              loginStyles.inputField,
+                              ( Platform.OS === 'ios' ) && loginStyles.inputFieldIOS
+                         ]}
+                         selectionColor="white"
+
+                         //TODO onChange, value
+
+                         autoCapitalize="none"
+                         autoCorrect={ false }
+                    />
+
+                    <Text style={ loginStyles.label }>Contraseña</Text>
+                    <TextInput
+                         placeholder="*********"
+                         placeholderTextColor="rgba(255,255,255,0.5)"
+                         keyboardType="email-address"
+                         underlineColorAndroid="white"
+                         style={[ 
+                              loginStyles.inputField,
+                              ( Platform.OS === 'ios' ) && loginStyles.inputFieldIOS
+                         ]}
+                         selectionColor="white"
+
+                         //TODO onChange, value
+
+                         autoCapitalize="none"
+                         autoCorrect={ false }
+                    />
+
+                    {/* Boton login */}
+                    <View style={ loginStyles.buttonContainer }>
+                         <TouchableOpacity
+                              activeOpacity={ 0.8 }
+                              style={ loginStyles.button }
+                         >
+                              <Text style={ loginStyles.buttonText }>Login</Text>
+                         </TouchableOpacity>
+                    </View>
+
+                    {/* Crear una nueva cuenta */}
+                    <View style={ loginStyles.newUserContainer }>
+                         <TouchableOpacity
+                              activeOpacity={ 0.8 }
+                              onPress={ () => console.log('Press') }
+                         >
+                              <Text style={ loginStyles.buttonText }>Nueva cuenta </Text>
+                         </TouchableOpacity>
+                    </View>
+               </View>
           </>
      )
 }
--- 10-productos-app/src/theme/loginTheme.tsx
+++ 10-productos-app/src/theme/loginTheme.tsx
@@ -1,15 +1,50 @@
 import { StyleSheet } from "react-native";
 
 export const loginStyles = StyleSheet.create({
-      title: {
-          color: 'white',
-          fontSize: 30,
-          fontWeight: 'bold',
-          margin: 20
-      },
-      label: {
-           marginTop: 25,
-           color: 'white',
-           fontWeight: 'bold'
-      }    
+    formContainer: {
+        flex: 1,
+        paddingVertical: 30,
+        justifyContent: 'center',
+        height: 600,
+        marginBottom: 50
+    },
+    title: {
+        color: 'white',
+        fontSize: 30,
+        fontWeight: 'bold',
+        margin: 20
+    },
+    label: {
+        marginTop: 25,
+        color: 'white',
+        fontWeight: 'bold'
+    },
+    inputField: {
+        color: 'white',
+        fontSize: 20
+    },
+    inputFieldIOS: {
+        borderBottomColor: 'white',
+        borderStartWidth: 2,
+        paddingBottom: 4
+    },
+    buttonContainer: {
+        alignItems: 'center',
+        marginTop: 50
+    },
+    button: {
+        borderWidth: 2,
+        borderColor: 'white',
+        paddingHorizontal: 20,
+        paddingVertical: 5,
+        borderRadius: 100
+    },
+    buttonText: {
+        fontSize: 20,
+        color: 'white'
+    },
+    newUserContainer: {
+        alignItems: 'flex-end',
+        marginTop: 10
+    }
 });
