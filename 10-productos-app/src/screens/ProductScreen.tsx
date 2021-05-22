8c7831bfb0fe4ee7ebade79779077afed3155aa9
Eraldo Vicente <eraldovicente2@gmail.com>
2021-05-21 22:37:10 -0300

    Diseno de la pantalla de producto

--- 10-productos-app/src/screens/ProductScreen.tsx
+++ 10-productos-app/src/screens/ProductScreen.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { Text, View } from 'react-native';
+import { Button, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
 
 import { StackScreenProps } from '@react-navigation/stack';
 import { ProductsStackParams } from '../navigator/ProductsNavigator';
@@ -17,8 +17,70 @@ export const ProductScreen = ({ navigation, route }: Props) => {
      }, []);
 
      return (
-          <View>
-               <Text>{ id } { name }</Text>
+          <View style={ styles.container }>
+
+               <ScrollView>
+
+                    <Text style={ styles.label }>Nombre del producto:</Text>
+                    <TextInput
+                         placeholder="Producto"
+                         style={ styles.textInput }
+                         // TODO:
+                         // value
+                         // onChangeText
+                    />
+
+                    {/* Picker / Selector */}
+                    <Text style={ styles.label }>Categoria:</Text>
+                         
+                    <Button
+                         title="Guardar"
+                         // TODO: Por hacer
+                         onPress={ () => {} }
+                         color="#5856D6"
+                    />
+
+                    <View style={{ flexDirection: 'row', justifyContent: 'center', marginTop: 30 }}>
+                         <Button
+                              title="Cámara"
+                              // TODO: Por hacer
+                              onPress={ () => {} }
+                              color="#5856D6"
+                         />
+
+                         <View style={{ width: 10 }} />
+
+                         <Button
+                              title="Galería"
+                              // TODO: Por hacer
+                              onPress={ () => {} }
+                              color="#5856D6"
+                         />
+                    </View>
+
+               </ScrollView>
+
           </View>
      )
 }
+
+const styles = StyleSheet.create({
+     container: {
+          flex: 1,
+          marginTop: 10,
+          marginHorizontal: 20
+     },
+     label: {
+          fontSize: 18
+     },
+     textInput: {
+          borderWidth: 1,
+          paddingHorizontal: 10,
+          paddingVertical: 5,
+          borderRadius: 20,
+          borderColor: 'rgba(0,0,0,0.2)',
+          height: 45,
+          marginTop: 5,
+          marginBottom: 15
+     }
+});
