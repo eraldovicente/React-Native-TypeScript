4e0e8b70da786e08b8349c6b5e7cb4afa045b29d
Eraldo Vicente <eraldovicente2@gmail.com>
2021-05-23 16:45:03 -0300

    Crear producto y actualizar - Llamar  el backend

--- 10-productos-app/src/screens/ProductScreen.tsx
+++ 10-productos-app/src/screens/ProductScreen.tsx
@@ -48,14 +48,15 @@ export const ProductScreen = ({ navigation, route }: Props) => {
           
      }
 
-     const saveOrUpdate = () => {
+     const saveOrUpdate = async() => {
 
           if ( id.length > 0 ) {
                updateProduct( categoriaId, nombre, id ); 
           } else {
                
                const tempCategoriaId = categoriaId || categories[0]._id;
-               addProduct( tempCategoriaId, nombre );
+               const newProduct = await addProduct( tempCategoriaId, nombre );
+               onChange( newProduct._id, '_id' );
           }
      }
 
@@ -102,7 +103,7 @@ export const ProductScreen = ({ navigation, route }: Props) => {
                     />
 
                     {
-                         ( id.length > 0 ) && (
+                         ( _id.length > 0 ) && (
 
                               <View style={{ flexDirection: 'row', justifyContent: 'center', marginTop: 30 }}>
                                    <Button
